c0be8cbac3cd802823c798eb7220945edbaa9ba3
edgawill <edgar-william@hotmail.com>
2022-03-31 16:49:53 +0000

    function that measures the size of a binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_size - function that measures the size of a binary tree
  *
- * @tree: the tree
+ * @tree: the tre
  * Return: Return the height
  */
 size_t binary_tree_size(const binary_tree_t *tree)
